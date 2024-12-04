7d8800b9b83c9cd6b67858b53743a304c92da1fd
antoneni2000 <neniscaaa@yahoo.com>
2024-12-04 07:08:27 +0200

    ok

--- hpli.php
+++ hpli.php
@@ -13,30 +13,32 @@
 		<header>
         <header>
       
-      <nav class="navbar">
-
-         <div class="branding">   
-   <a href="HomePage.html"><img id="logo" src="images/logo2.png" alt="logo"></a> 
-       </div>
-
-    <div class="navbar-menu">
-     <ul>
-       <li><a href="HomePage.html">Acasa</a></li>
-       <li><a href="educational.html">Stiai ca?</li>
-       <li><a href = "galerie.html">Galerie</a></li>
-       <li><a href="Programari.php">Programeaza-te</a></li>
-       <li><a href="contact.html">Contacteaza-ne!</a></li>
-       <li><a href="logout.php">Iesi din cont</a></li>
-     </ul>
-   </div>
-   
-   <a href="#" class="toggle-button">
-     <span class="bar"></span>
-     <span class="bar"></span>
-     <span class="bar"></span>
-     </a>
-
-   </nav>
+        <nav class="navbar">
+            <div class="branding">
+                <a href="HomePage.html"><img id="logo" src="images/logo2.png" alt="logo"></a>
+            </div>
+            <div class="navbar-menu">
+                <ul>
+                    <li><a href="HomePage.html" id="nav-home">Acasă</a></li>
+                    <li><a href="educational.html" id="nav-did-you-know">Știai că?</a></li>
+                    <li><a href="galerie.html" id="nav-gallery">Galerie</a></li>
+                    <li><a href="Programari.php" id="nav-schedule">Programează-te</a></li>
+                    <li><a href="contact.html" id="nav-contact">Contactează-ne!</a></li>
+                    <li><a href="LogIn.php" id="nav-login">Intră în cont</a></li>
+                    <li><a href="sign_up.php" id="nav-signup">Fă-ți un cont</a></li>
+                </ul>
+            </div>
+            <select id="language-selector">
+                <option value="ro">Română</option>
+                <option value="en">English</option>
+                <option value="fr">Français</option>
+            </select>
+            <a href="#" class="toggle-button">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </a>
+        </nav>
 </header>
 
 	</body>
